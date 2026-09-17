de8dbdc6b543db810d9edda235cf30401b579f13
Steve the Killer <steve@thekiller.net>
2026-09-16 21:44:53 -0700

    v1.7.2: install the Chocolatey package through Install-ChocolateyPackage

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,17 +1,14 @@
 $ErrorActionPreference = 'Stop'
-$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$version  = $env:ChocolateyPackageVersion
+$version = $env:ChocolateyPackageVersion
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    fileFullPath   = Join-Path $toolsDir 'KillerScan.exe'
+    fileType       = 'exe'
+    silentArgs     = '/silent'
     url64bit       = "https://github.com/SteveTheKiller/KillerScan/releases/download/v$version/KillerScan.exe"
     checksum64     = 'REPLACE_HASH'
     checksumType64 = 'sha256'
+    validExitCodes = @(0)
 }
 
-Get-ChocolateyWebFile @packageArgs
-
-$installer = Start-Process -FilePath $packageArgs.fileFullPath -ArgumentList '/silent' -Wait -PassThru -WindowStyle Hidden
-if ($installer.ExitCode -ne 0) { throw "KillerScan installation failed with exit code $($installer.ExitCode)." }
-New-Item -ItemType File -Path ($packageArgs.fileFullPath + '.ignore') -Force | Out-Null
+Install-ChocolateyPackage @packageArgs
